fbcb59f42b55702a1315b9f359b09fd573c46ed0
Roy <ruoyumao@yahoo.com>
2022-04-13 18:43:27 +0900

    person model validation added

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,18 +1,18 @@
 class Person < ApplicationRecord
   # validation filed
   # TODO: refactor to person_form object for reuse
-  # TODO: name 1-32
-  # set default name value
-  validates :name, presence: true
-  # TODO: verify phone number
-  validates :phone_number, presence: true
-  # TODO: check if it is email using regular expression to check
-  validates :email, uniqueness: true, presence: true
-  # TODO: change timedate to date
-  validates :available_on, presence: true
-  # todo: refactor to use enum
-  validates :user_type, presence: true, numericality: { only_integer: true }
+  validates :name, length: { minimum: 1, maximum: 32 }, presence: true
+  validates :phone_number, numericality: true, length: { minimum:10, maximum: 12 }, uniqueness:true, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "invalid email" },
+  uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 128 }, presence: true
+  validate :available_date_must_be_in_the_future
+  validates :user_types, presence: true, numericality: { only_integer: true }
 
   # warning: this will generate a instance method "child?", raise_conflict_error
   enum :user_types, [ :child_type, :adult_type, :parent_type ]
+
+  def available_date_must_be_in_the_future
+    errors.add(:available_on, "can't be in the past") if
+    !available_on.blank? and available_on < Date.today
+  end
 end
