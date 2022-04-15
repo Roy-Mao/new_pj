2e2af92b8ee7982eca8a074715fac34fe2c24afe
Roy <ruoyumao@yahoo.com>
2022-04-15 15:14:09 +0900

    adjust validation method syntax of person class

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,15 +1,14 @@
 class Person < ApplicationRecord
-  # TODO: refactor to person_form object for reuse
-  validates :name, length: { minimum: 1, maximum: 32 }, allow_nil: true
 
-  validates :phone_number, numericality: true, length: { minimum:10, maximum: 12 }, uniqueness:true, presence: true
+  validates :name, length: { in: 1..32 }, allow_nil: true
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "invalid email" },
-  uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 128 }, allow_nil: true
-
-  validate :available_date_must_be_in_the_future
+  validates :phone_number, numericality: true, length: { in: 10..12 }, uniqueness:true, presence: true
 
   validates :user_type, presence: true, numericality: { only_integer: true }, allow_nil: true
+
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "invalid email", allow_nil: true
+
+  validate :available_date_must_be_in_the_future
 
   # warning: this will generate a instance method "child?", raise_conflict_error
   enum :user_type, [ :child_type, :adult_type, :parent_type ]
