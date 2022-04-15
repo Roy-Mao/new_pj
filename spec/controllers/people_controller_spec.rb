6b6dc99d1c8030d825841d85ce7e8381b1456200
Roy <ruoyumao@yahoo.com>
2022-04-15 15:55:13 +0900

    add people_controller put method test

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe PeopleController do
       expect(response).to have_http_status(302)
     end
   end
+
+  describe "PUT update/:id" do
+    before do
+      allow_any_instance_of(CodeVerifier).to receive(:valid_code?).and_return(true)
+    end
+
+    context "with valid parameters" do
+      let(:person_attributes) { { name: "fake_name", phone_number: "1234567890" } }
+      it "hoge hoge" do
+        person = Person.create(person_attributes)
+        binding.pry
+        put person_path(person)
+        expect(response).to have_http_status(200)
+        # params: { verification_code: "1234", email: "fake_name@yahoo.com", available_on: Date.today, user_type: 1 }
+      end
+    end
+  end
 end
